55e593ce826f3cc07b3f0b36e22a1c0cd6da7b28
Scott Schwab <scottschwab86@gmail.com>
2024-06-07 13:48:41 -0500

    Complete fetch post request to save current weather snapshot to db

--- client/src/components/CurrentWeather/CurrentWeather.js
+++ client/src/components/CurrentWeather/CurrentWeather.js
@@ -2,12 +2,31 @@ import styles from './CurrentWeather.module.css';
 
 const CurrentWeather = ({weatherData}) => {
 
+  const handleSaveSnapshotClick = () => {
+
+    // JSON.stringify data, then post it to back end to be saved
+    let weatherSnapshot = {
+      "temperature": weatherData.currentTemp,
+      "timestamp": weatherData.currentTime,
+      "weather_code": weatherData.currentWeatherCode
+    }
+
+    fetch('http://localhost:5555/api/saved-weather-data', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(weatherSnapshot)
+    })
+  }
+
   return (
     <div className={styles.containerCard}>
       <p>Current Weather</p>
       <p>{weatherData.currentTime}</p>
       <p>{weatherData.currentTemp}</p>
       <p>{weatherData.currentWeatherCode}</p>
+      <button onClick={handleSaveSnapshotClick}>Save Snapshot to DB</button>
     </div>
   )
 }
