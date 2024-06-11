5844cfcbbda5ec6429ef9dd058a3f989be91a879
Scott Schwab <scottschwab86@gmail.com>
2024-06-11 13:17:51 -0500

    Get rid of weather code label and leave only icon

--- client/src/components/WeatherCard/WeatherCard.js
+++ client/src/components/WeatherCard/WeatherCard.js
@@ -2,10 +2,7 @@ import styles from './WeatherCard.module.css';
 import weatherCodeImgs from '../../weathercodes.json';
 
 const WeatherCard = ({time, temp, weatherCode}) => {
-  // console.log("RAW TIME", time)
   let timeObj = new Date(time);
-
-  // console.log("TIMEOBJ", timeObj)
 
   let timeOfDay = (timeObj.getHours() < 6 || timeObj.getHours() > 18) ? "night" : "day"
 
@@ -21,7 +18,7 @@ const WeatherCard = ({time, temp, weatherCode}) => {
     <div className={styles.containerCard}>
       <p>{timeObj.toLocaleString()}</p>
       <p>{temp}° F</p>
-      <p>{weatherCode}</p>
+      {/* <p>{weatherCode}</p> */}
       <img src={weatherCodeImg} alt="weather-code-icon"></img>
     </div>
   )
